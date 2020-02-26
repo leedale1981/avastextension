45cc86b3ed7a037a622fa22bf81a206bba270e8f
Lee Dale <lee.dale@ofqual.gov.uk>
2020-02-26 10:39:13 +0000

    Initial commit

---
+++ chrome/precache-manifest.ebd4244d6397ccff4e63b3126c01549a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7919559d1c07c94ec1666a89daaa0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00fca9380660eb596c9d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "af782d3ba3e7a2fba436",
+    "url": "/static/js/2.ce1678e0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ce1678e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00fca9380660eb596c9d",
+    "url": "/static/js/main.ac734873.chunk.js"
+  },
+  {
+    "revision": "5a7a97eeb207faf893df",
+    "url": "/static/js/runtime-main.18d2c966.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
